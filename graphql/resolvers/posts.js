44fec37a7a23259bd3ed669493dd03c75e6567d6
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-03-03 17:58:48 -0500

    Add register user and login mutation Add getPosts, getPost queries Add createPost mutation Add validation and authentication feature

---
+++ graphql/resolvers/posts.js
@@ -0,0 +1,41 @@
+const Post = require('../../models/Post');
+const checkAuth = require('../../utils/check-auth');
+
+module.exports = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error('Post not found');
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createAt: new Date().toDateString(),
+      });
+
+      const post = await newPost.save();
+      return post;
+    },
+  },
+};
