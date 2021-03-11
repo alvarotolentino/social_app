b0f3c5a90158b18374053fba5541af822c04f0c2
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-03-11 00:58:11 -0500

    Add authentication route Persist authentication token in local storage

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 import { AuthProvider } from './context/auth';
+import AuthRoute from './util/AuthRoute';
 
 import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
@@ -18,8 +19,8 @@ function App() {
         <Container>
           <MenuBar />
           <Route exact path='/' component={Home}></Route>
-          <Route exact path='/login' component={Login}></Route>
-          <Route exact path='/register' component={Register}></Route>
+          <AuthRoute exact path='/login' component={Login}></AuthRoute>
+          <AuthRoute exact path='/register' component={Register}></AuthRoute>
         </Container>
       </Router>
     </AuthProvider>
